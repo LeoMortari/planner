dcd30553506581c57decd581e43cd7def8e094a6
LeoMortari <leo.mortari.forinn@gmail.com>
2024-07-12 11:33:38 -0300

    Adiciona primeiros endpoints de busca, registro. Adiciona bruno e sql

---
+++ src/main/java/com/rockeseat/planner/trip/Trip.java
@@ -0,0 +1,51 @@
+package com.rockeseat.planner.trip;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "trips")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+
+    @Column(nullable = false, name = "starts_at")
+    private LocalDateTime startsAt;
+
+    @Column(nullable = false, name = "ends_at")
+    private LocalDateTime endsAt;
+
+    @Column(nullable = false, name = "is_confirmed")
+    private Boolean isConfimed;
+
+    @Column(nullable = false, name = "owner_name")
+    private String ownerName;
+
+    @Column(nullable = false, name = "owner_email")
+    private String ownerEmail;
+
+    public Trip(TripRequestPayload data){
+        this.destination = data.destination();
+        this.isConfimed = false;
+        this.ownerEmail = data.owner_email();
+        this.ownerName = data.owner_name();
+        this.startsAt = LocalDateTime.parse(data.starts_at(), DateTimeFormatter.ISO_DATE_TIME);
+        this.endsAt = LocalDateTime.parse(data.ends_at(), DateTimeFormatter.ISO_DATE_TIME);
+
+    }
+}
---
+++ src/main/java/com/rockeseat/planner/trip/TripCreateResponse.java
@@ -0,0 +1,6 @@
+package com.rockeseat.planner.trip;
+
+import java.util.UUID;
+
+public record TripCreateResponse(UUID tripId) {
+}
